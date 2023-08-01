e379228f7127502a2099fe8ce766b71c0030f2c8
Izwa-Akemi <sirokumaorange1129@gmail.com>
2023-08-01 18:53:44 +0900

    jqeury授業

---
+++ js_ex/count/common.js
@@ -0,0 +1,41 @@
+//要素の取得
+let result = document.getElementById("text-result");
+let buttonReset = document.getElementById("change-text1");
+let buttonAdd = document.getElementById("change-text2");
+let buttonSub = document.getElementById("change-text3");
+
+//値を文字列で定数にしておく
+const BASE_TEXT = "値："
+
+//計算結果を格納する変数
+let resultValue = 0;
+
+//値を表示するメソッド
+function changeResultText(){
+    result.innerHTML = BASE_TEXT + resultValue;
+}
+
+//値を計算するメソッド
+function calculateResultValue(setValue){
+    resultValue = resultValue + setValue;
+}
+
+//リセットボタンを押したとき
+buttonReset.addEventListener('click',()=>{
+    //値を初期化
+    resultValue = 0;
+    changeResultText();
+})
+
+//１を加算ボタンを押したとき
+buttonAdd.addEventListener('click',()=>{
+    calculateResultValue(1);
+    changeResultText();
+})
+
+
+//１を減算ボタンを押したとき
+buttonSub.addEventListener('click',()=>{
+    calculateResultValue(-1);
+    changeResultText();
+})
